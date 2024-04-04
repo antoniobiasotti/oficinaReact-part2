a19cee6c2c9f7e455a428d2ff61152549cbe0f7e
Antônio Biasotti Amorim <102745879+antoniobiasotti@users.noreply.github.com>
2024-04-04 00:08:27 +0000

    add route.js + response and status msg

---
+++ src/app/api/country/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import data from "./data.json";
+
+export async function GET() {
+  try {
+    const response = data.map((country) => {
+      return {
+        name: country.name,
+        population: country.population,
+        region: country.region,
+        capital: country.capital,
+        flag: country.flags.svg,
+        code: country.numericCode,
+      };
+    });
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
