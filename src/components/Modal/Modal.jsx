5607043b21aa22f3cf7f52c4c4cd6a6e75ba270c
antoniobiasotti <abamorim36@gmail.com>
2024-04-04 19:08:26 -0300

    Styling Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -15,21 +15,65 @@ export const Modal = ({
 }) => {
     return (
         <section className={styles.modalBody}>
-            <button onClick={closeModal}>
-                <ArrowLeft size={22} />
-                Back
-            </button>
-            <div className={styles.image}>
-                <Image
-                src={imagePath}
-                width={154}
-                height={70}
-                alt="flag"
-                className={styles.img}
-                />
-            </div>
             <div>
-                <h2>Nome do País</h2>
+                <button onClick={closeModal}>
+                    <ArrowLeft size={22} />
+                    Back
+                </button>
+            </div>
+            <div className={styles.modalContent}>
+                <div className={styles.image}>
+                    <Image
+                    src={imagePath}
+                    width={584}
+                    height={450}
+                    alt="flag"
+                    className={styles.img}
+                    />
+                </div>
+                <div className={styles.info}>
+                    <h2>Nome do País</h2>
+                    <div>
+                        <p>
+                            <span>Native name: </span>
+                            {region}
+                        </p>
+                        <p>
+                            <span>Population: </span>
+                            {population.toLocaleString("pt-BR")}
+                        </p>
+                        <p>
+                            <span>Region: </span>
+                            {region}
+                        </p>
+                        <p>
+                            <span>Sub Region: </span>
+                            {capital}
+                        </p>
+                        <p>
+                            <span>Capital: </span>
+                                {region}
+                        </p>
+                    </div>
+                    <div>    
+                        <p>
+                            <span>Top level Domain: </span>
+                            {region}
+                        </p>
+                        <p>
+                            <span>Currencies: </span>
+                            {region}
+                        </p>
+                        <p>
+                            <span>Languages: </span>
+                            {region}
+                        </p>
+                        <p>
+                            <span>Border Countries: </span>
+                            {region}
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
     );
